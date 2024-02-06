92a618270c05cd00b0c5c26b06935555a820e98e
Emile <133158597+Elie00000@users.noreply.github.com>
2024-02-06 12:18:36 +0100

    Update bdd.py

--- bdd.py
+++ bdd.py
@@ -5,18 +5,26 @@ def create_user_db():
     conn = sqlite3.connect('user.db')
     c = conn.cursor()
 
-    # Créer la table
-    c.execute('''CREATE TABLE users
-                 (email text primary key, password text)''')
+    try:
+        # Vérifier si la table existe
+        c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users'")
+        if c.fetchone() is None:
+            # Créer la table si elle n'existe pas
+            c.execute('''CREATE TABLE users
+                         (email text primary key, password text)''')
 
-    # Insérer un utilisateur
-    hashed_password = generate_password_hash('Islande-2404', method='sha256')
-    c.execute("INSERT INTO users VALUES ('emile.alberici.snir@gmail.com', ?)", (hashed_password,))
+        # Insérer un utilisateur
+        email = 'emile.alberici.snir@gmail.com'  # à obtenir de manière sécurisée
+        password = 'Islande-2404'  # à obtenir de manière sécurisée
+        hashed_password = generate_password_hash(password, method='sha256')
+        c.execute("INSERT INTO users VALUES (?, ?)", (email, hashed_password))
 
-    # Enregistrer (commit) les modifications
-    conn.commit()
-
-    # Fermer la connexion
-    conn.close()
+        # Enregistrer (commit) les modifications
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Une erreur s'est produite : {e}")
+    finally:
+        # Fermer la connexion
+        conn.close()
 
 create_user_db()
