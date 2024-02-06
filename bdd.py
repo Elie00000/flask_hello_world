72ad6beae06673a35eb8d6538e56459f557c6a54
Emile <133158597+Elie00000@users.noreply.github.com>
2024-02-06 11:05:03 +0100

    Create bdd.py

---
+++ bdd.py
@@ -0,0 +1,22 @@
+import sqlite3
+from werkzeug.security import generate_password_hash
+
+def create_user_db():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+
+    # Créer la table
+    c.execute('''CREATE TABLE users
+                 (email text primary key, password text)''')
+
+    # Insérer un utilisateur
+    hashed_password = generate_password_hash('testexemple', method='sha256')
+    c.execute("INSERT INTO users VALUES ('exemple@gmail.com', ?)", (hashed_password,))
+
+    # Enregistrer (commit) les modifications
+    conn.commit()
+
+    # Fermer la connexion
+    conn.close()
+
+create_user_db()
